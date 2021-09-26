d192ff1cb1d447e8949548138d22520f9fa88e42
Riswan <73796371+riswan-indra@users.noreply.github.com>
2021-09-26 22:16:04 +0700

    hari 1 ( mencetak string ke console )

---
+++ day-1(printing)/latihan.py
@@ -0,0 +1,12 @@
+print("Day 1 Input Output") /bin/python3
+
+nama = input("Nama : ")
+alamat = input("Alamat : ")
+t_lahir = input("Alamat Kelahiran: ")
+tgl_lahir = input("Tanggal Kelahiran : ")
+bln_lahir = input("Bulan Kelahiran : ")
+thn_lahir = input("Tahun Kelahiran :")
+print("\n")
+print("Nama     : " + nama)
+print("Alamat   : " + alamat)
+print("Tempat, Tanggal Lahir : " + t_lahir + "," + tgl_lahir + " " + bln_lahir + " " + thn_lahir)
---
+++ day-1(printing)/main.py
@@ -0,0 +1,56 @@
+print("hello world!")
+
+####### 1.1 #######
+
+# print("Day 1 - python Print Function")
+# print("The funcion  is Declarate like this :")
+# print("print('what to print')")
+
+
+####### 1.2 (debug) #######
+
+# print("new lines can be created with a backslash and n.")
+
+# print(Day 1 - String Manipulation")
+# print("Day 1 - string manipulation") <--
+
+# print("string concatenation is done with the "+" sign,")
+# print('string concatenation  is done with the "+" sign.') <--
+
+#     print('e.g. print("hello" + "world")')
+# print('e.g. print("hello" + "world")') <--
+
+# print(("new lines can be created with a backslash and n."))
+# print("new lines can be created with a backslash and n.") <--
+
+
+
+# input
+# print(input("berapakah usia anda sekarang?"))
+# print("Haii"  + input('siapa nama anda?')) 
+
+# interaktif input
+# print(len(input("nama anda?")))
+
+# variabel
+# nama = input("nama anda?")
+# print(nama)
+
+# a = input("a :")
+# b = input("b :")
+
+# c = a
+# a = b
+# b = c
+# print("a : " + a)
+# print("b : " + b)
+
+# challanges
+input("isi form berikut : \n")
+nama = input("Nama anda ?")
+umur = input("Berapa umur anda sekarang ?")
+alamat = input("Lokasi anda sekarang ?")
+print("form data anda : ")
+print("Nama: " + nama)
+print("Umur: "+umur)
+print("Alamat: " + alamat) 
